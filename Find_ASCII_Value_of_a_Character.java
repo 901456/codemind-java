0a4596aafb1787cf464487eab2bcb704e03ef568
Anjali korada <97424576+901456@users.noreply.github.com>
2022-09-23 14:48:10 +0530

    import java.util.Scanner; class AChhh {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char n;         n=sc.nextLine().charAt(0);
            System.out.print((int)(n));
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class AChhh
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char n;
+        n=sc.nextLine().charAt(0);
+        
+        System.out.print((int)(n));
+    }
+}
